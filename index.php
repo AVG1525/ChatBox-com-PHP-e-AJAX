dbb3c4952003b09bbddc1a2785e5d03b2c60682b
Aron <aronvinicius@live.com.pt>
2019-04-17 16:08:08 -0300

    Index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>ChatBox</title>
 		
-		<link rel="stylesheet" href="css-aki.css">
+		<link rel="stylesheet" href="css.css">
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 		<script type="text/javascript" src="ajax.js"></script>
 		<script type="text/javascript">
